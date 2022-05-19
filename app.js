76b15e00cb6e38c0121028dce4c3117139454451
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-05-19 17:58:52 +0500

    jave file

---
+++ app.js
@@ -0,0 +1,207 @@
+
+
+// Declare three varibales in one statement=======================
+var first,second,third;
+first=1;
+second=2;
+third=3;
+
+// alert("this is me " + second);
+
+
+
+// Legal Varibales============================
+
+var my_First; 
+var third;
+var myThird; //Its camelCase
+var number1; //First one always should be letter
+
+
+// Illegal Variables=============================
+
+
+// var 69;  //Can't start with numbers
+// var @first; //Special characters not allowed except underscore and dollar
+// var mythird; //Always use camelCase
+// var alert; //Can't use reserved keywords
+// var fifth one; //Can't contain spaces
+
+
+// Variables for strings===================================
+
+var username;
+
+var myName="Huzaifa Habib";
+
+// Script for display hello world on alert box=============
+var message;
+message="Hello World";
+// alert("" + message);
+
+
+// Script for display student bio through multiple alert boxes=======
+
+var age=19;
+var collegeName="Ait";
+var course="Web and Mobile application develpoment";
+
+// alert("Welcome " + myName);
+// alert("My age " + age);
+// alert("My college " + collegeName);
+// alert("My course name " + course);
+
+
+// Program of fortune========================
+
+var numberOfChildern=2;
+var partnerName="xyz";
+var geographicLocation="Pakistan";
+var jobTitle="Software Engineer";
+
+// alert("You will be a " +jobTitle + " in " + geographicLocation + " and married to " + partnerName + 
+// " with " + numberOfChildern + " kids " );
+
+
+// Variable for email=========================
+
+var email="fake@gmail.com.";
+// alert("My email address is " + email );
+
+
+// Variable for book===========================
+
+var book="A Smart Way To Learn JavaScript.";
+// alert("I am trying to learn from the book " + book);
+
+
+                        // Variables For Numbers=================================
+
+
+// Variable called age===========
+
+var age=19;
+// alert("I am " + age + " years old. ")
+
+
+// Program for online clothing store===================
+
+var visitorName="Ali Khan";
+var product="Shirts";
+var quantity=5;
+
+                        // Maths Expression==============================
+// For addition===================
+var x=3;
+var y=5;
+var sum=x+y;
+// alert("The sum of 3 and 5 after addition is " + sum);
+
+// For Subtraction=========================
+
+var x=3;
+var y=5;
+var sum=x-y;
+// alert("The sum of 3 and 5 after subtration is " + sum);
+
+// For division====================
+var x=3;
+var y=5;
+var sum=x/y;
+// alert("The sum of 3 and 5 after division is " + sum);
+
+
+// For Modulus======================
+
+var x=3;
+var y=5;
+var sum=x%y;
+// alert("Remainder of dividing 3 by 5 is " + sum);
+
+
+
+
+// Maths Expression===============
+
+var number=10;
+console.log (++number);
+console.log (7+number);
+console.log (--number);
+console.log (3/number);
+
+
+// Script to store ticket price===================
+
+var oneTicket=900;
+var totalCostOfFiveTickets=5*oneTicket;
+// alert("" + totalCostOfFiveTickets);
+
+
+// Multiplication table of five===============
+
+var a=5
+
+console.log(5*1);
+console.log(5*2);
+console.log(5*3);
+console.log(5*4);
+console.log(5*5);
+console.log(5*6);
+console.log(5*7);
+console.log(5*8);
+console.log(5*9);
+console.log(5*10);
+
+
+
+                // Script for temperature converter======================
+
+
+// var celcius=40;
+// var f=20;
+// console.log (conversion=celcius=(f-32)*5/9);
+
+
+
+                // Program for shopping cart===========================
+
+
+var priceOfItem1=200;
+var priceOfItem2=300;
+var orderQuantitiyOfItem1=10;
+var orderQuantitiyOfItem2=5;
+var shippingCharges=500;
+
+var item1Cost=priceOfItem1*orderQuantitiyOfItem1;
+var item2Cost=priceOfItem2*orderQuantitiyOfItem2;
+
+var totalCost=item1Cost+item2Cost+shippingCharges;
+// alert("Result " + totalCost);
+
+
+                        //Program for marksheet==========================
+                        
+var totalMarks=1000;
+var marksObtained=700;
+var total=(marksObtained/totalMarks)*100;
+// alert("Percentage is " + total);
+
+
+
+                    // Program to convert currency in PKR====================
+
+var oneDollar=104;
+var tenDollar=10*oneDollar;
+var oneRayal=29;
+var twentyFiveRayal=25*oneRayal;
+
+console.log (tenDollar+twentyFiveRayal);
+
+
+
+                    // Program for airthmetic====================
+
+var a=20
+var expression=[(a+5)*10]/2;
+console.log ([(a+5)*10]/2 )
+   
